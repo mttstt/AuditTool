815124a57c95647b6bd1b814bc0da8e9ee99d176
mttstt <mtt_stt@yahoo.it>
2017-05-22 20:43:38 +0200

    release 0.0.1

---
+++ go.bat
@@ -0,0 +1,17 @@
+SET dir1=C:\Users\matteo\Documents\GitHub\AuditTool
+del /Q "%dir1%"\AuditTool.json
+
+curl https://www.meteorkitchen.com/api/getapp/json/Tqq4JcxsuGEBZrben  -o "%dir1%"\AuditTool.json
+
+rmdir AuditTool /S /Q
+mkdir emptyfolder
+robocopy emptyfolder AuditTool /purge
+rmdir emptyfolder
+rmdir AuditTool
+
+meteor-kitchen ./AuditTool.json ./AuditTool
+
+copy  "%dir1%"\files\hacking\fileinput.js "%dir1%"\AuditTool\client\styles\framework\bootstrap3-plugins\bootstrap-fileinput\fileinput.js /Y
+
+cd ./AuditTool
+meteor --settings ../production-settings.json --port 5000
