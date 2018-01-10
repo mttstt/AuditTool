193d332d28810b5fc833a9937479e56a15eef063
mttstt <mtt_stt@yahoo.it>
2018-01-10 08:51:38 +0100

    Update go-linux.sh

--- go-linux.sh
+++ go-linux.sh
@@ -16,7 +16,7 @@ wget http://www.meteorkitchen.com/api/getapp/json/Tqq4JcxsuGEBZrben -O AuditTool
 
 meteor-kitchen http://www.meteorkitchen.com/api/getapp/json/Tqq4JcxsuGEBZrben AuditTool
 
-cp /home/mtt/AuditTool/files/hacking/fileinput.js /home/mtt/AuditTool/AuditTool/client/styles/framework/bootstrap3-plugins/bootstrap-fileinput/fileinput.js
+# cp /home/mtt/AuditTool/files/hacking/fileinput.js /home/mtt/AuditTool/AuditTool/client/styles/framework/bootstrap3-plugins/bootstrap-fileinput/fileinput.js
 
 cd AuditTool
 
