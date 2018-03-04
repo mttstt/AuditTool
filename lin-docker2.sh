433e8570270627f908dc0b6b4220c916a5d62512
mttstt <mtt_stt@yahoo.it>
2018-03-04 22:02:47 +0100

    Create lin-docker2.sh

---
+++ lin-docker2.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+# -n: cancella tutto (container, images,..) e ricrea la struttura 
+
+#newgrp docker
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -n|--new)
+    echo "Clean ALL"
+
+        echo "Ferma tutti i containers"
+        docker container stop $(docker ps -a -q)
+
+        echo "Elimina tutti i container"
+        docker container rm $(docker ps -a -q)
+
+        echo "Elimina tutti i volumi"
+        docker volume rm $(docker volume ls -q)
+
+        echo "Crea volumi"
+        docker volume audittoolvolume
+        docker volume jsreportvolume
+        docker volume mongovolume
+        docker volume audittool_dir
+
+        # Per copiare i dati devo prima creare un container dummy temporaneo
+        echo "Copio file lib"
+        docker container create --name dummy -v audittoolvolume:/tmp/files/lib hello-world
+        docker cp ~/AuditTool/files/lib dummy:/tmp/files
+        docker rm dummy
+
+        echo "Elimina tutte le immagini"
+        docker rmi $(docker images -a -q)
+
+        echo "Elimina tutte le reti tranne quella di default 172.17.0.0/16"
+        docker network rm $(docker network ls -q)
+
+        echo "Crea nuova sottorete mttlan"
+        docker network create --subnet=192.168.2.0/16 mttlan
+
+    shift # past argument
+    shift # past value
+    ;;
+    -b|--bye)
+    echo "bye"
+ 
+    echo "bye"
+    shift # past argument
+    shift # past argument
+    ;;
+    *)    # unknown option
+    shift # past argument
+    ;;
+esac
+done
+
+echo "Inizio"
+
+# sudo systemctl stop firewalld.service
+# echo "In order to list images: docker images -a"
+# echo "In order to delete container: docker rm <ID container>"
+# echo "In order to delete: docker rmi <ID Images>"
+# echo "In order to delete all: docker system prune"
+# echo "In order to install Mongodb: docker run --name mongo-meteor -d mongo"
+# echo "In order to create network: docker network create --subnet=192.168.1.0/16 mttlan"
+
+rm -rf .git
+
+cd ~/AuditTool
+
+cp -R files/ /tmp
+
+rm -fR AuditTool
+
+wget http://www.meteorkitchen.com/api/getapp/json/Tqq4JcxsuGEBZrben -O AuditTool.json
+
+meteor-kitchen AuditTool.json AuditTool
+
+cd  AuditTool
+
+meteor build --architecture=os.linux.x86_64 --directory ./
+
+############################################################## for docker ##########################################################
+# Ferma tutti i containers
+docker container stop $(docker ps -a -q)
+
+# Elimina tutti i container
+docker container rm $(docker ps -a -q)
+
+# Elimina l'immagine audittol
+docker rmi $(docker images mttstt/audittool -q) -f
+
+# Avvia container
+docker run -d --net mttlan --ip 192.168.2.1 --name jsreport -p 80:5488 --restart always -v jsreportvolume:/jsreport jsreport/jsreport
+docker run -d --net mttlan --ip 192.168.2.2 --name meteor-mongo -v mongovolume:/data/db mongo
+
+cp ../files/.dockerignore  ~/AuditTool/AuditTool
+
+docker run -it \
+ --net mttlan --ip 192.168.2.5 \
+ --name audittool \
+ -e MONGO_URL=mongodb://192.168.2.2 \
+ -e ROOT_URL=http://192.168.2.5 \
+ -v audittoolvolume:/tmp/files/lib \
+ -v /home/mtt/AuditTool/AuditTool:/bundle \
+ -p 8080:80 \
+ abernix/meteord:node-8.9.3-base \
+ /bin/bash
+
+echo "Docker images ls:"
+docker image ls
+
+docker ps -q | xargs docker inspect --format '{{ .Id }} - {{ .Name }} - {{ .NetworkSettings.IPAddress }}'
+################################################################################################################################
