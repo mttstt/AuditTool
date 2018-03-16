b832b3f9e29299965ef10e8c90cc621842d333ae
mttstt <mtt_stt@yahoo.it>
2018-03-16 12:36:03 +0100

    Update lin-docker.sh

--- lin-docker.sh
+++ lin-docker.sh
@@ -269,7 +269,7 @@ case $key in
 
     -u|--dockerup)
         echo "docker up"
-        JRSI=`ip -4 addr show scope global dev docker0 | grep inet | awk '{print $2}' | cut -d / -f 1`
+        HOST_IP=`ip -4 addr show scope global dev docker0 | grep inet | awk '{print $2}' | cut -d / -f 1`
         export JRSI=$HOST_IP && passwdAD=$passwdAD && docker-compose up
     shift # past argument
     shift # past argument
