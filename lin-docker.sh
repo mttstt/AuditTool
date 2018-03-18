094522785c704ad75fa79538ee5b1c37e6dd2607
mttstt <mtt_stt@yahoo.it>
2018-03-18 17:02:40 +0100

    Update lin-docker.sh

--- lin-docker.sh
+++ lin-docker.sh
@@ -13,14 +13,11 @@
 #
 # -h, --help        Help
 # -m, --meteor      Launch meteor, no Docker (without Docker)
-# -l, --launch      Launch container AuditTool (old without Docker Hub)
-# -t, --tar         Create tar meteor (old without Docker Hub)
-# -o, --onbuild     Build audittool docker image
+# -b, --build       Build audittool docker image
 # -p, --push        Push audittool image to Docker Hub [password Docker HUB] [New versione release]
 # -u, --dockerup    Docker-compose up
-# -n, --new         Create tar meteor and Run container (old without Docker Hub)
 # -d, --delete      Delete all (containers, images, volumes, networks) (old without Docker Hub)
-# -r, --reload      Delete all (containers, images, volues, networks) and create all again (old without Docker Hub)
+# 
 #
 # Useful command for ubuntu 17: newgrp docker
 #
@@ -33,114 +30,20 @@ passwd="$2"
 release="$3"
 
 case $key in
-    -r|--reload)
-        echo "Recreate all"
-        echo "Ferma tutti i containers"
-        docker container stop $(docker ps -a -q)
-        echo "Elimina tutti i container"
-        docker container rm $(docker ps -a -q)
-        echo "Elimina tutti i volumi"
-        docker volume rm $(docker volume ls -q)
-        echo "Crea volumi"
-        docker volume create audittoolvolume
-        docker volume create jsreportvolume
-        docker volume create mongovolume
-        # Per copiare i dati devo prima creare un container dummy temporaneo
-        echo "Copio file lib"
-        docker container create --name dummy -v audittoolvolume:/tmp/files/lib hello-world
-        docker cp ~/AuditTool/files/lib dummy:/tmp/files
-        docker rm dummy
-        echo "Elimina tutte le immagini"
-        docker rmi $(docker images -a -q)
-                    
-        # Avvia container
-        docker run -d --name jsreport -p 5488:5488 --restart always -v jsreportvolume:/jsreport jsreport/jsreport
-        docker run -d --name meteor-mongo -p 27017:27017 -v mongovolume:/data/db mongo
-        ############################################################## create tar meteor ###################################################
-        cd ~/AuditTool        
-        if [ ! -f ~/AuditTool/.gitignore ]; then
-            echo ".gitignore not found!"
-        else
-            rm .gitignore
-        fi        
-        rm -rf .git
-        cp -R files/ /tmp
-        sudo rm -fR AuditTool
-        wget http://www.meteorkitchen.com/api/getapp/json/Tqq4JcxsuGEBZrben -O AuditTool.json
-        meteor-kitchen AuditTool.json AuditTool
-        cd  AuditTool
-        meteor build --architecture=os.linux.x86_64 ./
-        ############################################################## for docker ##########################################################
-        # Se attivo ferma ed elimina il container audittool  
-        docker stop audittool || true && docker rm audittool || true
-        JRSI=$(docker inspect jsreport --format '{{ .NetworkSettings.IPAddress }}')
-        docker run \
-         -it \
-         --name audittool \
-         --link "meteor-mongo:db" \
-         -e "MONGO_URL=mongodb://db" \
-         -e ROOT_URL=http://127.0.0.1 \
-         -e "jsReportServerIp=$JRSI" \
-         -e "passwdAD=$passwd" \
-         -v audittoolvolume:/tmp/files/lib \
-         -v /home/mtt/AuditTool/AuditTool:/bundle \
-         -p 8080:80 \
-         abernix/meteord:node-8.9.3-base \
-         /bin/bash
-        echo "Docker images ls:"
-        docker image ls
-        docker ps -q | xargs docker inspect --format '{{ .Id }} - {{ .Name }} - {{ .NetworkSettings.IPAddress }}'
-        ################################################################################################################################
-
-    shift # past argument
-    shift # past value
-    ;;
-    
-    -t|--tar)
-        echo "Build Meteor tar"
-        cd ~/AuditTool
-        if [ ! -f ~/AuditTool/.gitignore ]; then
-            echo ".gitignore not found!"
-        else
-            rm .gitignore
-        fi
-        rm -rf .git
-        cp -R files/ /tmp
-        sudo rm -fR AuditTool
-        wget http://www.meteorkitchen.com/api/getapp/json/Tqq4JcxsuGEBZrben -O AuditTool.json
-        meteor-kitchen AuditTool.json AuditTool
-        cd  AuditTool
-        meteor build --architecture=os.linux.x86_64 ./    
-    shift # past argument
-    shift # past argument
-    ;;
- 
     -d|--delete)
         echo "Delete All"
         echo "Ferma tutti i containers"
-        docker container stop $(docker ps -a -q)
-        echo "Elimina tutti i container"
-        docker container rm $(docker ps -a -q)
+        docker-compose down
         echo "Elimina tutti i volumi"
-        docker volume rm $(docker volume ls -q)
-        echo "Crea volumi"
-        docker volume create audittoolvolume
-        docker volume create jsreportvolume
-        docker volume create mongovolume
-        # Per copiare i dati devo prima creare un container dummy temporaneo
-        echo "Copio file lib"
-        docker container create --name dummy -v audittoolvolume:/tmp/files/lib hello-world
-        docker cp ~/AuditTool/files/lib dummy:/tmp/files
-        docker rm dummy
+        docker volume prune -f
         echo "Elimina tutte le immagini"
         docker rmi $(docker images -a -q)
-
     shift # past argument
     shift # past argument
     ;;
     
-    -n|--new)
-        echo "Create tar meteor e run docker"
+    -b|--build)
+        echo "build"
         cd ~/AuditTool
         if [ ! -f ~/AuditTool/.gitignore ]; then
             echo ".gitignore not found!"
@@ -153,72 +56,8 @@ case $key in
         wget http://www.meteorkitchen.com/api/getapp/json/Tqq4JcxsuGEBZrben -O AuditTool.json
         meteor-kitchen AuditTool.json AuditTool
         cd  AuditTool
-        meteor build --architecture=os.linux.x86_64 ./
-        ############################################################## for docker ##########################################################
-        # Se attivo ferma ed elimina il container audittool  
-        docker stop audittool || true && docker rm audittool || true
-        JRSI=$(docker inspect jsreport --format '{{ .NetworkSettings.IPAddress }}')
-        docker run \
-         -it \
-         --name audittool \
-         --link "meteor-mongo:db" \
-         -e "MONGO_URL=mongodb://db" \
-         -e ROOT_URL=http://127.0.0.1 \
-         -e "jsReportServerIp=$JRSI" \
-         -e "passwdAD=$passwd" \
-         -v audittoolvolume:/tmp/files/lib \
-         -v /home/mtt/AuditTool/AuditTool:/bundle \
-         -p 8080:80 \
-         abernix/meteord:node-8.9.3-base \
-         /bin/bash
-        echo "Docker images ls:"
-        docker image ls
-        docker ps -q | xargs docker inspect --format '{{ .Id }} - {{ .Name }} - {{ .NetworkSettings.IPAddress }}'
-        ################################################################################################################################
-
-    shift # past argument
-    shift # past argument
-    ;;
-    
-    -l|--launch)
-        echo "Run AuditTool"
-        docker stop audittool || true && docker rm audittool || true
-        JRSI=$(docker inspect jsreport --format '{{ .NetworkSettings.IPAddress }}')
-        docker run \
-         -it \
-         --name audittool \
-         --link "meteor-mongo:db" \
-         -e "MONGO_URL=mongodb://db" \
-         -e ROOT_URL=http://127.0.0.1 \
-         -e "jsReportServerIp=$JRSI" \
-         -e "passwdAD=$passwd" \
-         -v audittoolvolume:/tmp/files/lib \
-         -v /home/mtt/AuditTool/AuditTool:/bundle \
-         -p 8080:80 \
-         abernix/meteord:node-8.9.3-base \
-         /bin/bash
-
-    shift # past argument
-    shift # past argument
-    ;;
-    
-    -o|--onbuild)
-        echo "Onbuild"
-        cd ~/AuditTool
-        if [ ! -f ~/AuditTool/.gitignore ]; then
-            echo ".gitignore not found!"
-        else
-            rm .gitignore
-        fi
-        rm -rf .git
-        cp -R files/ /tmp
-        sudo rm -fR AuditTool
-        wget http://www.meteorkitchen.com/api/getapp/json/Tqq4JcxsuGEBZrben -O AuditTool.json
-        meteor-kitchen AuditTool.json AuditTool
-        cd  AuditTool
-        echo "FROM abernix/meteord:node-8.9.3-devbuild" > Dockerfile
-        docker build -t mttstt/audittool .
-       
+        echo "FROM abernix/meteord:node-8.9.3-onbuild" > Dockerfile
+        ocker-compose build --force-rm --no-cache               
     shift # past argument
     shift # past argument
     ;;
@@ -254,7 +93,6 @@ case $key in
     shift # past argument
     ;;  
 
-
     -u|--dockerup)
         echo "docker up"
         HOST_IP=`ip -4 addr show scope global dev ens33 | grep inet | awk '{print $2}' | cut -d / -f 1`
@@ -287,15 +125,11 @@ case $key in
         echo ""
         echo "-h, --help        Help"
         echo "-m, --meteor      Launch meteor, no Docker (without Docker)"
-        echo "-l, --launch      Launch container AuditTool (old without Docker Hub)"
-        echo "-t, --tar         Create tar meteor (old without Docker Hub)"
-        echo "-o, --onbuild     Build audittool docker image"
+        echo "-b, --build       Build audittool docker image"
         echo "-p, --push        Push audittool image to Docker Hub [password Docker HUB] [New versione release]"
-        echo "-u, --dockerup    Docker-compose up
-        echo "-n, --new         Create tar meteor and Run container (old without Docker Hub)"
+        echo "-u, --dockerup    Docker-compose up"
         echo "-d, --delete      Delete all (containers, images, volumes, networks) (old without Docker Hub)"
-        echo "-r, --reload      Delete all (containers, images, volues, networks) and create all again (old without Docker Hub)"
-        echi ""   
+        echo ""   
       
     shift # past argument# Options:
     ;;
